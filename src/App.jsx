65841256cd9f28a31ab09ab31864e54e179fac1c
kim_haaga <kim.haaga@live.se>
2021-04-14 20:27:41 +0200

    Add state and fix spelling in test file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from "react"
 
 class App extends Component {
+  state = {
+    geolocation: {}
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      debugger
+      this.setState({ geolocation: position.coords })
+    })
+  }
+
   render() {
     return(
       <div>
