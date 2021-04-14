889dc844939c1bb324be67e2c0b4140ea690d875
kim_haaga <kim.haaga@live.se>
2021-04-14 20:48:06 +0200

    Add API links to opencagedata and openweathermap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import axios from "axios"
 
 class App extends Component {
   state = {
@@ -6,7 +7,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
+    navigator.geolocation.getCurrentPosition(async position => {
+      let { latitude, longitude } = position.coords
+      let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=6c8da41014b24daabdd53ec40e96481e`)
+      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely&appid=9e94ebf8c79351a0faa6bb802de835ad`)
       debugger
       this.setState({ geolocation: position.coords })
     })
