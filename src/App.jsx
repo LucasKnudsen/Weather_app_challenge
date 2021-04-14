78b1d75fb5bcb1bd716856ea433d8333a88293ab
kim_haaga <kim.haaga@live.se>
2021-04-14 21:28:41 +0200

    Add function for location & weather response data

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ class App extends Component {
       let { latitude, longitude } = position.coords
       let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=6c8da41014b24daabdd53ec40e96481e`)
       let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely&appid=9e94ebf8c79351a0faa6bb802de835ad`)
+
+      let weatherInfo = {
+        city: locationResponse.data.results[0].components.postal_city,
+        temp: weatherResponse.data.current.temp
+      }
+      this.setState({ location: weatherInfo })
       debugger
       this.setState({ geolocation: position.coords })
     })
