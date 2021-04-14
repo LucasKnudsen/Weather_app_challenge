881bf1727a7e5e5a3694c4e341511c5d46a0c3e9
kim_haaga <kim.haaga@live.se>
2021-04-14 11:47:14 +0200

    Add project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import react, { Component } from "react"
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Sup</h1>
+      </div>
+    )
+  }
+}
+export default App;
