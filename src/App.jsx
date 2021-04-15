86bac03a6654b2940e7ff988d5d9de4774949ccf
arunbhalli <tomarbhalli.com>
2021-04-15 10:04:16 +0200

    api key added in env file

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ class App extends Component {
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(async position => {
-      let { latitude, longitude } = position.coords
-      let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=6c8da41014b24daabdd53ec40e96481e`)
-      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely&appid=9e94ebf8c79351a0faa6bb802de835ad`)
+      let openCageKey= process.env.REACT_APP_OPEN_CAGE_KEY
+      let openWeatherKey= process.env.REACT_APP_OPEN_WEATHER_KEY
+      let {latitude, longitude } = position.coords
+      let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${openCageKey}`)
+      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely&appid=${openWeatherKey}`)
 
       let weatherInfo = {
         city: locationResponse.data.results[0].components.postal_city,
