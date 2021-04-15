6c65a9c4c21fe2cbdf8ebaba0fbd0faf50ef51d3
kim_haaga <kim.haaga@live.se>
2021-04-15 15:41:26 +0200

    Add intercept to test 'city' key

--- cypress/integration/userCanGetLocalWeather.feature.js
+++ cypress/integration/userCanGetLocalWeather.feature.js
@@ -1,29 +1,39 @@
 describe("weather info for user's location", () => {
   beforeEach(() => {
-    cy.intercept("https://api.openweathermap.org/data/2.5/**",{fixture:'weather_response.json'})
-    cy.intercept("https://api.opencagedata.com/v1/json/**",{fixture:'location_response.json'})
-  })
-  it("is expected to be displayed on initial render", () => {
-    cy.visit("/", {
-      onBeforeLoad(window) {
-        const stubLocation = {
-          coords: {
-            latitude: 55.7842,
-            longitude: 12.4518,
-          },
-        };
-        cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
-          (callback) => {
-            return callback(stubLocation);
-          }
-        );
-      },
+    cy.intercept("https://api.opencagedata.com/v1/json/**", {
+      fixture: "location_response_city.json",
     });
 
-    cy.get("[data-cy=weather-display]").within(() => {
-      cy.get("[data-cy=temp]").should("contain", "17°C");
-      cy.get("[data-cy=location]").should("contain", "Virum");
-      cy.get("[data-cy=weather-type]").should("contain", "Clouds");
+    beforeEach(() => {
+      cy.intercept("https://api.openweathermap.org/data/2.5/**", {
+        fixture: "weather_response.json",
+      });
+      cy.intercept("https://api.opencagedata.com/v1/json/**", {
+        fixture: "location_response.json",
+      });
+    });
+    it("is expected to be displayed on initial render", () => {
+      cy.visit("/", {
+        onBeforeLoad(window) {
+          const stubLocation = {
+            coords: {
+              latitude: 55.7842,
+              longitude: 12.4518,
+            },
+          };
+          cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
+            (callback) => {
+              return callback(stubLocation);
+            }
+          );
+        },
+      });
+
+      cy.get("[data-cy=weather-display]").within(() => {
+        cy.get("[data-cy=temp]").should("contain", "17°C");
+        cy.get("[data-cy=location]").should("contain", "Virum");
+        cy.get("[data-cy=weather-type]").should("contain", "Clouds");
+      });
     });
   });
 });
