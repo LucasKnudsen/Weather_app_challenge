b6e7025f96981c5108da0cf6860117e0114ce1eb
kim_haaga <kim.haaga@live.se>
2021-04-16 13:18:23 +0200

    Add comment 'weekend challenge

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import axios from "axios"
 import { Container } from "semantic-ui-react"
 
+// weekend challenge
 class App extends Component {
   state = {
     geolocation: {},
